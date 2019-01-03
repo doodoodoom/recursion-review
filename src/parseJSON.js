ec6f9ff766e585f29b0ad41c0a3d2796e28cf18e
doodoodoom <madridmatt17@gmail.com>
2019-01-02 16:25:50 -0800

    Write pseudo-code for parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,7 +1,26 @@
 // this is what you would do if you were one to do things the easy way:
 // var parseJSON = JSON.parse;
 
+/*
+
+'{"apple": 10,"banana": 20,"orange": [1, 2, 3],"pickle": null}'
+*/
+
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // Input : string containing json obj
+  // Output : object
+  // Constraints : na
+  // Edge Cases : empty string, array, obj, undefined, func
+
+  var isDigits = function(strVal) {
+    
+  };
+
+  var toPrimitive = function(strVal) {
+    if (strVal === 'null') {
+      return null;
+    }
+  };
+  
 };
